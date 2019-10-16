6145714d9f5143704c48ff626252d5cccf475826
Alžběta Adámková <bety.adamkova@gmail.com>
2019-10-16 19:14:59 +0200

    prvni

--- dataloader.py
+++ dataloader.py
@@ -1,54 +1,66 @@
-import numpy as np
+import numpy
 import matplotlib.pyplot as plt
-
+import matplotlib.image as img
 import torch
 import glob
 from skimage.io import imread
 from torch.utils import data
 
-class Dataloader(Dataset):
+class Dataloader(data.Dataset):
     """Nacitani dat"""
 
-    def __init__(self,split="trenink",path_to_data='/Desktop/data'):
+    def __init__(self,path_to_data='/Users/betyadamkova/Desktop/data_vse/stage1_train'):
         
-        self.split=split
-        self.path=path_to_data + '/' + split 
-        
-        #self.pic1 = mpimg.imread('pic1.png')
-        #self.mask1 = mpimg.imread('mask1.png')
+        self.path=path_to_data + '/' 
      
-        self.file_list=[]
-        self.lbls=[]
-
-     for k in range(6):
-        #prochazeni slozky
-        files=glob.glob(self.path + '/'+ str(k) +'/*png')
-        self.file_list.extend(files)
-        self.lbls.extend([k]*len(files))
+        self.maska_list=[]
+        self.orig_list=[]
         
-        self.num_of_imgs=len(self.file_list)
+        self.slozka= glob.glob(path_to_data + '/*') 
         
+        for i in self.slozka:
+            nazev_maska = glob.glob(i + '/masks/*.png')
+            nazev_orig = glob.glob(i + '/images/*.png')
+            print(nazev_orig)
+            orig = imread(nazev_orig[0])
+            
+            self.orig_list.append(orig)
+        
+            
+            maska = numpy.zeros((256,256))
+            
+            for k in nazev_maska:
+                 #prochazeni slozky
+                maska_k = imread(k)
+                maska = maska + maska_k
+                
+                
+            self.maska_list.append(maska)
+           
+            """   
+            imgplot = plt.imshow(maska)
+            plt.show()
+            img.imsave('maska.png',maska,cmap="gray")     
+            """ 
+                
     
     def __len__(self):
-        return len(self.num_of_imgs)
+        return len(self.maska_list)
 
 
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx):     
         
-        img=imread(self.file_list[idx])
-        img=torch.Tensor(img.astype(np.float32)/255-0.5)
-        lbl=torch.Tensor(np.array(self.lbls[idx]).astype(np.float32))
-        return img,lbl
+        #img_mask = torch.Tensor(img_mask.astype(np.float32)/255-0.5)
+        #lbl=torch.Tensor(np.array(self.lbls[idx]).astype(np.float32))
+      
+        return self.maska_list[idx],self.orig_list[idx]
 
-loader = Dataloader(split='trenink')
-trainloader= data.Dataloader(loader,batch_size=2, num_workers=0, shuffle=True,drop_last=True)
-
-
+loader = Dataloader()
+#trainloader= data.Dataloader(loader,batch_size=2, num_workers=0, shuffle=True,drop_last=True)
 
 
-
-
+"""
 
 for it,(batch,lbls) in enumerate(trainloader): ### you can iterate over dataset (one epoch)
   print(batch)
@@ -57,23 +69,4 @@ for it,(batch,lbls) in enumerate(trainloader): ### you can iterate over dataset 
   plt.imshow(batch[0,:,:].detach().cpu().numpy())
   break
         
-        
-        
-        
-  """
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks])
-        landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
- """
+"""     
