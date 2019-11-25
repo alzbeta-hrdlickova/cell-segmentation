38287ab2df146a281ffdf056723e4b406a1c5813
Alžběta Adámková <bety.adamkova@gmail.com>
2019-11-25 15:06:58 +0100

    nmbmbmn

---
+++ train_cycle.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.core.debugger import set_trace
+import torch
+import torch.nn as nn
+
+from torch.utils import data
+from dataloader import DataLoader
+from Unet import Unet
+
+batch=1
+
+loader = DataLoader(split='trenink') 
+trainloader= torch.utils.data.DataLoader(loader,batch_size=batch, num_workers=0, shuffle=True,drop_last=True)
+  
+loader = DataLoader(split='test')
+testloader= torch.utils.data.DataLoader(loader,batch_size=1, num_workers=0, shuffle=True,drop_last=True)
+
+#device="cuda" if torch.cuda.is_available() else "cpu"
+#model=model.to(device)
+
+net=Unet()          #.cuda()
+
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+it=-1
+for epoch in range(1000):
+  for k,(data,lbl) in enumerate(trainloader):
+    it+=1
+    
+    data=data       #.cuda()
+    lbl=lbl         #.cuda()
+    
+    #data.requires_grad=True
+    #lbl.requires_grad=True
+    
+    optimizer.zero_grad()   # zero the gradient buffers
+    
+    net.train()
+    
+    output=net(data)
+    #output=F.sigmoid(output)
+
+    datanp=data.cpu().detach().numpy()
+    
+    output=net(data)
+    outputnp=data.cpu().detach().numpy() #prevedeni zpet pro zobrazeni
+     
+    #MSE=torch.nn.MSELoss(size_avarage=False)
+    loss=torch.mean((lbl-output)**2)
+    loss.backward()  # pocitani gradientu
+    optimizer.step() # update parametrs
+    
+        
+"""      
+#jednou za určitý počet epoch je potřeba změřit výsledky
+
+    if it%50==0:  
+      for kk,(data,lbl) in enumerate(testloader):
+          
+          
+          #vykreslit obrazky
+          plt.plot(data,np.array(lbl).ravel(),'*')
+          plt.show()
+ 
+#to stejné jako při trénování ale bez výpočtu gradientu úpravy vah
+
+"""
