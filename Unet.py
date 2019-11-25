d0a85e57b67c1a23e13e1ab29f192d36d1fb1583
Alžběta Adámková <bety.adamkova@gmail.com>
2019-11-25 12:56:00 +0100

    pouzivana funkce Unet

--- Unet.py
+++ Unet.py
@@ -1,64 +1,150 @@
-import numpy
-import matplotlib.pyplot as plt
-from IPython.core.debugger import set_trace
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F  
-import torch.optim as optim
-import torchvision
-from torchvision import transforms
-
-import glob
-import os
-from skimage.io import imread
-
-from torch.utils import data
-from dataloader import DataLoader
-
-batch=16
-
-loader = DataLoader(split='trenink') 
-trainloader= torch.utils.data.DataLoader(loader,batch_size=batch, num_workers=0, shuffle=True,drop_last=True)
-
-  
-loader = DataLoader(split='test')
-testloader= torch.utils.data.DataLoader(loader,batch_size=1, num_workers=0, shuffle=True,drop_last=True)
+import torch.nn.functional as F
+import numpy as np
+from torch.nn import init
 
 
-net=Unet().cuda()
+class unetConv2(nn.Module):
+    def __init__(self, in_size, out_size,filter_size=3,stride=1,pad=1,do_batch=1):
+        super().__init__()
+        
+        self.do_batch=do_batch
+    
+        self.conv=nn.Conv2d(in_size, out_size,filter_size,stride,pad)
+        self.bn=nn.BatchNorm2d(out_size,momentum=0.1)
 
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+        dov=0.1
+        self.do=nn.Sequential(nn.Dropout(dov),nn.Dropout2d(dov))
+
+    def forward(self, inputs):
+        outputs = self.conv(inputs)
+        
+        if self.do_batch:
+            outputs = self.bn(outputs)          
+        outputs=F.relu(outputs)
+        outputs = self.do(outputs)
+
+        return outputs
+
+class unetConvT2(nn.Module):
+    def __init__(self, in_size, out_size,filter_size=3,stride=2,pad=1,out_pad=1):
+        super().__init__()
+        self.conv = nn.ConvTranspose2d(in_size, out_size,filter_size,stride=stride, padding=pad, output_padding=out_pad)
+        
+    def forward(self, inputs):
+        outputs = self.conv(inputs)
+        outputs=F.relu(outputs)
+        return outputs
+    
+
+class unetUp(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(unetUp, self).__init__()
+
+        self.up = unetConvT2(in_size, out_size ) 
+        #self.up=nn.UpsamplingBilinear2d(scale_factor=2)
+
+    def forward(self, inputs1, inputs2):
+         
+        inputs2 = self.up(inputs2)
+
+        return torch.cat([inputs1, inputs2], 1)
 
 
-it=-1
-for epoch in range(1000):
-  for k,(data,lbl) in enumerate(trainloader):
-    it+=1
-    
-    data=data.cuda()
-    lbl=lbl.cuda()
-    
-    
-    data.requires_grad=True
-    lbl.requires_grad=True
-    
-    optimizer.zero_grad()   # zero the gradient buffers
-    
-    net.train()
-    
-    output=net(data)
-    output=F.sigmoid(output)
-    
-    loss=dice_loss(output, lbl) ### tady budete mít MSE asi místo toho
-    
-    loss.backward()  ## claculate gradients
-    optimizer.step() ## update parametrs
-    
-    
-"""      
-    if it%50==0:  ###jednou za určitý počet epoch je potřeba změřit výsledky
-      for kk,(data,lbl) in enumerate(testloader):
- 
-#to stejné jako při trénování ale bez výpočtu gradientu úpravy vah
-"""
+class Unet(nn.Module):
+    def __init__(self, filters=[16, 32, 64],in_size=1,out_size=1):
+        super().__init__()
+        
+        self.out_size = out_size
+        
+        self.filters = filters
+        
+        
+        self.conv1 = nn.Sequential(unetConv2(in_size, filters[0]),unetConv2(filters[0], filters[0]),unetConv2(filters[0], filters[0]))
+
+        if len(filters)>=3:
+            self.conv2 =  nn.Sequential(unetConv2(filters[0], filters[1] ),unetConv2(filters[1], filters[1] ),unetConv2(filters[1], filters[1] ))
+
+        if len(filters)>=4:
+            self.conv3 = nn.Sequential(unetConv2(filters[1], filters[2] ),unetConv2(filters[2], filters[2] ),unetConv2(filters[2], filters[2] ))
+
+        if len(filters)>=5:
+            self.conv4 = nn.Sequential(unetConv2(filters[2], filters[3] ),unetConv2(filters[3], filters[3] ),unetConv2(filters[3], filters[3] ))
+
+
+        self.center = nn.Sequential(unetConv2(filters[-2], filters[-1] ),unetConv2(filters[-1], filters[-1] ))
+
+        # upsampling
+        if len(filters)>=5:
+            self.up_concat4 = unetUp(filters[4], filters[4] )
+            self.up_conv4=nn.Sequential(unetConv2(filters[3]+filters[4], filters[3] ),unetConv2(filters[3], filters[3] ))
+
+        
+        if len(filters)>=4:
+            self.up_concat3 = unetUp(filters[3], filters[3] )
+            self.up_conv3=nn.Sequential(unetConv2(filters[2]+filters[3], filters[2] ),unetConv2(filters[2], filters[2] ))
+
+
+
+        if len(filters)>=3:
+            self.up_concat2 = unetUp(filters[2], filters[2] )
+            self.up_conv2=nn.Sequential(unetConv2(filters[1]+filters[2], filters[1] ),unetConv2(filters[1], filters[1] ))
+
+
+        self.up_concat1 = unetUp(filters[1], filters[1])
+        self.up_conv1=nn.Sequential(unetConv2(filters[0]+filters[1], filters[0] ),unetConv2(filters[0], filters[0],do_batch=0 ))
+        
+        
+        
+        self.final = nn.Conv2d(filters[0], self.out_size, 1)
+        
+
+        
+        for i, m in enumerate(self.modules()):
+            if isinstance(m, nn.Conv2d):
+                init.xavier_normal_(m.weight)
+                init.constant_(m.bias, 0)
+            
+     
+    def forward(self, inputs):
+        
+
+        conv1 = self.conv1(inputs)
+        x = F.max_pool2d(conv1,2,2)
+
+        if len(self.filters)>=3:
+            conv2 = self.conv2(x)
+            x = F.max_pool2d(conv2,2,2)
+
+        if len(self.filters)>=4:
+            conv3 = self.conv3(x)
+            x = F.max_pool2d(conv3,2,2)
+
+        if len(self.filters)>=5:
+            conv4 = self.conv4(x)
+            x = F.max_pool2d(conv4,2,2)
+
+        x = self.center(x)
+
+        if len(self.filters)>=5:
+            x = self.up_concat4(conv4, x)
+            x=self.up_conv4(x)
+        
+        if len(self.filters)>=4:
+            x = self.up_concat3(conv3, x)
+            x = self.up_conv3(x)
+
+        if len(self.filters)>=3:
+            x = self.up_concat2(conv2, x)
+            x=self.up_conv2(x)
+        
+        x = self.up_concat1(conv1, x)
+        x=self.up_conv1(x)
+        
+
+        x = self.final(x)
+        
+#        x=torch.tanh(x)
+#        sig=self.sm(final)
+        return x
