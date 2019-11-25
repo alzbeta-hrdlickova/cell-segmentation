8e93dfbe949d518721742ef37bc521e272c19080
Alžběta Adámková <bety.adamkova@gmail.com>
2019-11-25 15:32:42 +0100

    fve

--- train_cycle.py
+++ train_cycle.py
@@ -8,7 +8,8 @@ from torch.utils import data
 from dataloader import DataLoader
 from Unet import Unet
 
-batch=1
+
+batch=1  #batch alespon 16
 
 loader = DataLoader(split='trenink') 
 trainloader= torch.utils.data.DataLoader(loader,batch_size=batch, num_workers=0, shuffle=True,drop_last=True)
@@ -20,6 +21,7 @@ testloader= torch.utils.data.DataLoader(loader,batch_size=1, num_workers=0, shuf
 #model=model.to(device)
 
 net=Unet()          #.cuda()
+net.requires_grad=True
 
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
@@ -31,8 +33,8 @@ for epoch in range(1000):
     data=data       #.cuda()
     lbl=lbl         #.cuda()
     
-    #data.requires_grad=True
-    #lbl.requires_grad=True
+    data.requires_grad=True
+    lbl.requires_grad=True
     
     optimizer.zero_grad()   # zero the gradient buffers
     
@@ -51,18 +53,45 @@ for epoch in range(1000):
     loss.backward()  # pocitani gradientu
     optimizer.step() # update parametrs
     
-        
-"""      
-#jednou za určitý počet epoch je potřeba změřit výsledky
-
+  
+    #jednou za určitý počet epoch je potřeba změřit výsledky
+    #to stejné jako při trénování ale bez výpočtu gradientu úpravy vah
     if it%50==0:  
       for kk,(data,lbl) in enumerate(testloader):
           
+          net=Unet()          #.cuda()
+          net.requires_grad=True
+            
+          optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+            
+          it=-1
+          for epoch in range(1000):
+             for k,(data,lbl) in enumerate(trainloader):
+                 it+=1
+                
+                data=data       #.cuda()
+                lbl=lbl         #.cuda()
+                
+                data.requires_grad=True
+                lbl.requires_grad=True
+                
+                optimizer.zero_grad()   # zero the gradient buffers
+                    
+                net.eval()
+               
+                output=net(data)
+            
+                datanp=data.cpu().detach().numpy()
+                
+                output=net(data)
+                outputnp=data.cpu().detach().numpy() 
+                 
+                loss=torch.mean((lbl-output)**2)
+                loss.backward() 
+                optimizer.step() 
           
-          #vykreslit obrazky
-          plt.plot(data,np.array(lbl).ravel(),'*')
-          plt.show()
+          
+              #vykreslit obrazky
+              plt.imshow(outputnp)
+              plt.imshow(lbl) 
  
-#to stejné jako při trénování ale bez výpočtu gradientu úpravy vah
-
-"""
