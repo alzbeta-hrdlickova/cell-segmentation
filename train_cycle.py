e0b18f34309897065367f56e7d2253e2b0c55b40
Alžběta Adámková <bety.adamkova@gmail.com>
2019-12-02 18:07:12 +0100

    test cycle

--- train_cycle.py
+++ train_cycle.py
@@ -20,12 +20,12 @@ testloader= torch.utils.data.DataLoader(loader,batch_size=1, num_workers=0, shuf
 #device="cuda" if torch.cuda.is_available() else "cpu"
 #model=model.to(device)
 
-net=Unet()          #.cuda()
+net=Unet()      #.cuda()
 net.requires_grad=True
 
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
-#vytvorit list pro vysledky loss
+#vytvorit list pro vysledky loss, každou 50-tou hodnotu, vykreslit do grafu
 hodnoty_train=[]
 hodnoty_test=[]
 
@@ -45,19 +45,19 @@ for epoch in range(1000):
     net.train()
     
     output=net(data)
-    #output=F.sigmoid(output)
 
     datanp=data.cpu().detach().numpy()
     
     output=net(data)
-    outputnp=data.cpu().detach().numpy() #prevedeni zpet pro zobrazeni
+    outputnp=data.cpu().detach().numpy() #prevedeni pro zobrazeni
      
     #MSE=torch.nn.MSELoss(size_avarage=False)
     loss=torch.mean((lbl-output)**2)
     loss.backward()  # pocitani gradientu
     optimizer.step() # update parametrs
     
-  
+    
+
     #jednou za určitý počet epoch je potřeba změřit výsledky
     #to stejné jako při trénování ale bez výpočtu gradientu úpravy vah
     if it%50==0:  
@@ -73,29 +73,35 @@ for epoch in range(1000):
              for k,(data,lbl) in enumerate(trainloader):
                  it+=1
                 
-                data=data       #.cuda()
-                lbl=lbl         #.cuda()
+                 data=data       #.cuda()
+                 lbl=lbl         #.cuda()
                 
-                data.requires_grad=True
-                lbl.requires_grad=True
+                 data.requires_grad=True
+                 lbl.requires_grad=True
                 
-                optimizer.zero_grad()   # zero the gradient buffers
+                 optimizer.zero_grad()   # zero the gradient buffers
                     
-                net.eval()
+                 net.eval()
                
-                output=net(data)
+                 output=net(data)
             
-                datanp=data.cpu().detach().numpy()
+                 datanp=data.cpu().detach().numpy()
                 
-                output=net(data)
-                outputnp=data.cpu().detach().numpy() 
+                 output=net(data)
+                 outputnp=data.cpu().detach().numpy() 
                  
-                loss=torch.mean((lbl-output)**2)
-                loss.backward() 
-                optimizer.step() 
-          
-          
-              #vykreslit obrazky
-              plt.imshow(outputnp)
-              plt.imshow(lbl) 
- 
+                 loss=torch.mean((lbl-output)**2)
+                 ##loss.backward() 
+                 optimizer.step() 
+"""        
+#zapis hodnot do grafu     
+hodnoty_train.append(output[0,0,:,:].data.cpu().numpy())`
+hodnoty_test.append(lbl[0][0].data.cpu().numpy())`
+
+#vykreslit obrazky, puvodni a segmentovany
+plt.subplot(121)
+plt.imshow(outputnp)    #output[1][0].cpu().numpy()
+plt.subplot(122)
+plt.imshow(lbl)         #(data[1].data.cpu().permute(1,2,0).numpy()*0.5+0.5))
+
+"""
