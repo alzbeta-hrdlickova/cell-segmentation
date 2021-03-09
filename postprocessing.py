5ce66f948937fb8b780d2626934a4a14b59627bb
alzbeta-hrdlickova <56198872+alzbeta-hrdlickova@users.noreply.github.com>
2021-03-09 23:42:16 +0100

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -12,7 +12,6 @@ from torch.utils.data import random_split
 from sklearn.metrics import confusion_matrix
 from scipy.signal import argrelextrema
 
-
 path_to_file='images/final/'                #nacitani dat
 #final= glob.glob(path_to_file + '*')
 
@@ -23,42 +22,72 @@ plt.imshow(lbl0[0,0,:,:],cmap="gray")
 fig.add_subplot(1, 2, 2)
 output0=np.load('images/final/output0.npy')
 plt.imshow(output0[0,0,:,:],cmap="gray")
-           
 
+def dice_loss(output, lbl):
+  
+    smooth = 1.
+    iflat = lbl[0,0,:,:]
+    tflat = output[0,0,:,:]
+    intersection = (iflat * tflat).sum()
+    A_sum = np.sum(iflat).astype(np.float32 )
+    B_sum = np.sum(tflat).astype(np.float32 )
+    
+    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth) )
+#dice_loss(output0, lbl0)
+
+lbl_set=[]   
+output_set=[]   
+dice_set=[] 
+sensitivity_set=[] 
+specificity_set=[] 
+FP_set=[] 
+FN_set=[] 
+
+ 
 it=-1
 for fin in range(70):
-    it=+1
-    
+    it+=1
+    #print(it)
     lbl =np.load(path_to_file + 'lbl' + str(it) +'.npy') 
-    #lbl + int(it)=lbl
+    lbl_set.append(lbl[0,0,:,:])
+
     for x in range(lbl[0,0,:,:].shape[0]):
         for y in range(lbl[0,0,:,:].shape[1]):
+            """
             if lbl[0,0,:,:][x,y] < -0.45 :
-                lbl[0,0,:,:][x,y] == 0
+                lbl[0,0,:,:][x,y] = 1
             else:
-                lbl[0,0,:,:][x,y] == 1
-                
-    #np.amin(lbl, axis=None, out=None, keepdims=<no value>, initial=<no value>, where=<no value>)
-
-    output  =np.load(path_to_file + 'output' + str(it) +'.npy')
+                lbl[0,0,:,:][x,y] = 0     
+            """
+    output =np.load(path_to_file + 'output' + str(it) +'.npy')
+    output_set.append(output[0,0,:,:])
+    dice_dice=dice_loss(output,lbl)
+    dice_set.append(dice_dice)
+    final_dice=sum(dice_set)/70
+    """
     for x in range(output[0,0,:,:].shape[0]):
         for y in range(output[0,0,:,:].shape[1]):
             if output[0,0,:,:][x,y] < -0.35 :
-                output[0,0,:,:][x,y] == 0
+                output[0,0,:,:][x,y] = 1
             else:
-                output[0,0,:,:][x,y] == 1
-                
-    #np.amin(output, axis=None, out=None, keepdims=<no value>, initial=<no value>, where=<no value>)
-
-TP = np.sum(((lbl==1) & (output ==1)).astype(np.float32))
-FN = np.sum(((lbl==0) & (output ==0)).astype(np.float32))
-TN = 1 - FN
-FP = 1 - TP
-
-sensitivity = TP / (TP + FN)
-specificity = TN / (FP + TN)
+                output[0,0,:,:][x,y] = 0
+    """
+    TP = np.sum(((lbl >= (-0.45)) & (output >= (-0.35))).astype(np.float32))
+    FN = np.sum(((lbl < (-0.45)) & (output < (-0.35))).astype(np.float32))
+    TN = 1 - FN
+    FP = 1 - TP  
+    sensitivity = TP / (TP + FN)
+    specificity = TN / (FP + TN)
+    sensitivity_set.append(sensitivity)
+    specificity_set.append(specificity)
+    final_sensitivity=sum(sensitivity_set)/70
+    final_specificity=sum(specificity_set)/70
+    
+print(final_sensitivity)
+print(final_specificity)
+print(final_dice)
+      
 #precision =  TP/ (FP + TP)
 #accuracy = (TN + TP)/ (TN + FP + FN + TP)
-print(sensitivity)
-print(specificity)    
-    
+#Dice = 2*TP / ( 2*TP + FP + FN )
+#Jaccard = TP / ( TP + FP + FN )   
