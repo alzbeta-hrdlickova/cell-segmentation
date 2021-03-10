b490e9bd0251c72784b7f14058100aad103f647f
alzbeta-hrdlickova <56198872+alzbeta-hrdlickova@users.noreply.github.com>
2021-03-10 18:10:05 +0100

    Update sensitivity_specificity.py

--- sensitivity_specificity.py
+++ sensitivity_specificity.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 path_to_file='/Users/betyadamkova/Desktop/final/' 
 
 sensitivity_set=[] 
+specificity_set=[]
+accuracy_set=[]
+dice_set=[]
+jaccard_set=[]
 
 it=-1
 for fin in range(70):
@@ -12,7 +16,7 @@ for fin in range(70):
     
     data =np.load('/Users/betyadamkova/Desktop/final/data/' + 'data' + str(it) +'.npy') 
     data=data[0,0,:,:]
-    threshold1=np.mean(data)
+    threshold1=0.1      #najít optimální threshold
     for x in range(224):      
         for y in range(224):
             if data[x,y] >= threshold1:
@@ -23,20 +27,38 @@ for fin in range(70):
     
     output =np.load('/Users/betyadamkova/Desktop/final/output/' + 'output' + str(it) +'.npy')
     output=output[0,0,:,:]
-    threshold2=np.mean(output)
+    threshold2= -0.28
     for x in range(224):
         for y in range(224):
             if output[x,y] <= threshold2:
                 output[x,y] = 0
             else:
                 output[x,y] = 1
-    plt.imshow(output,cmap="gray")
+    #plt.imshow(output,cmap="gray")
           
     TP = np.sum(((data==1) & (output ==1)).astype(np.float32))
-    FN = np.sum(((data==0) & (output ==0)).astype(np.float32))
+    FN = np.sum(((data==1) & (output ==0)).astype(np.float32))
+    TN = np.sum(((data==0) & (output ==0)).astype(np.float32))
+    FP = np.sum(((data==0) & (output ==1)).astype(np.float32))
+    
     sensitivity = TP / (TP + FN)
+    specificity = TN / (TN + FP)
+    accuracy = (TN + TP)/ (TN + FP + FN + TP)
+    Dice = 2*TP / ( 2*TP + FP + FN )
+    Jaccard = TP / ( TP + FP + FN ) 
+    #precision =  TP/ (FP + TP)
+    
     sensitivity_set.append(sensitivity)
+    specificity_set.append(specificity)
+    accuracy_set.append(accuracy)
+    dice_set.append(Dice)
+    jaccard_set.append(Jaccard)
+    
     final_sensitivity=sum(sensitivity_set)/70
+    final_specificity=sum(specificity_set)/70
+    final_accuracy=sum(accuracy_set)/70
+    final_dice=sum(dice_set)/70
+    final_jaccard=sum(jaccard_set)/70
 
 fig = plt.figure()
 fig.add_subplot(1, 2, 1)   
@@ -45,9 +67,4 @@ fig.add_subplot(1, 2, 2)
 plt.imshow(output,cmap="gray")
 
 print(final_sensitivity)
-
-
-#precision =  TP/ (FP + TP)
-#accuracy = (TN + TP)/ (TN + FP + FN + TP)
-#Dice = 2*TP / ( 2*TP + FP + FN )
-#Jaccard = TP / ( TP + FP + FN ) 
+  
