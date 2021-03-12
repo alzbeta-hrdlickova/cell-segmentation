90f8cd8069fc03c4727eed5424a508ccfa3486e2
alzbeta-hrdlickova <56198872+alzbeta-hrdlickova@users.noreply.github.com>
2021-03-12 21:02:29 +0100

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -1,71 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.core.debugger import set_trace
+from skimage.filters import try_all_threshold
+from skimage.filters import threshold_otsu
+from scipy import ndimage as ndi
 
-import glob
-import os
-from skimage.io import imread
-from dataloader import DataLoader
-from Unet import Unet
-from torch.utils import data
-from torch.utils.data import random_split
-from sklearn.metrics import confusion_matrix
-from scipy.signal import argrelextrema
+from skimage.segmentation import watershed
+from skimage.feature import peak_local_max
 
-path_to_file='images/final/'                #nacitani dat
-#final= glob.glob(path_to_file + '*')
 
-fig = plt.figure()
-fig.add_subplot(1, 2, 1)   
-lbl0=np.load('images/final/lbl0.npy')
-plt.imshow(lbl0[0,0,:,:],cmap="gray")
-fig.add_subplot(1, 2, 2)
-output0=np.load('images/final/output0.npy')
-plt.imshow(output0[0,0,:,:],cmap="gray")
+def binar(data):
+    data=data[0,0,:,:]
+    #fig, ax = try_all_threshold(data, figsize=(10, 6), verbose=False)    #najít optimální threshold
+    threshold=threshold_otsu(data)
+    for x in range(224):      
+        for y in range(224):
+            if data[x,y] >= threshold:
+                data[x,y] = 1
+            else:
+                data[x,y] = 0
+    return data
 
-lbl_set=[]   
-output_set=[]   
-dice_set=[] 
-sensitivity_set=[] 
-specificity_set=[] 
-FP_set=[] 
-FN_set=[] 
-
- 
 it=-1
 for fin in range(70):
     it+=1
-    #print(it)
+    print(it)
 
-    output =np.load(path_to_file + 'output' + str(it) +'.npy')
-    output_set.append(output[0,0,:,:])
-    dice_dice=dice_loss(output,lbl)
-    dice_set.append(dice_dice)
-    final_dice=sum(dice_set)/70
-    """
-    for x in range(output[0,0,:,:].shape[0]):
-        for y in range(output[0,0,:,:].shape[1]):
-            if output[0,0,:,:][x,y] < -0.35 :
-                output[0,0,:,:][x,y] = 1
-            else:
-                output[0,0,:,:][x,y] = 0
-    """
-    TP = np.sum(((lbl >= (-0.45)) & (output >= (-0.35))).astype(np.float32))
-    FN = np.sum(((lbl < (-0.45)) & (output < (-0.35))).astype(np.float32))
-    TN = 1 - FN
-    FP = 1 - TP  
-    sensitivity = TP / (TP + FN)
-    specificity = TN / (FP + TN)
-    sensitivity_set.append(sensitivity)
-    specificity_set.append(specificity)
-    final_sensitivity=sum(sensitivity_set)/70
-    final_specificity=sum(specificity_set)/70
+    data =np.load('/Users/betyadamkova/Desktop/final/data/' + 'data' + str(it) +'.npy') 
+    binar_data=binar(data)
     
-print(final_sensitivity)
-print(final_specificity)
-print(final_dice)
-      
-#precision =  TP/ (FP + TP)
-#accuracy = (TN + TP)/ (TN + FP + FN + TP)
-#Dice = 2*TP / ( 2*TP + FP + FN )
-#Jaccard = TP / ( TP + FP + FN )   
+    output =np.load('/Users/betyadamkova/Desktop/final/output/' + 'output' + str(it) +'.npy')
+    binar_output=binar(output)
+    
+    image = np.logical_or(binar_output,binar_output)
+
+    distance = ndi.distance_transform_edt(image)
+    coords = peak_local_max(distance, footprint=np.ones((3, 3)), labels=image)
+    mask = np.zeros(distance.shape, dtype=bool)
+    mask[tuple(coords.T)] = True
+    markers, _ = ndi.label(mask)
+    labels = watershed(-distance, markers, mask=image)
+    
+fig, axes = plt.subplots(ncols=3, figsize=(9, 3), sharex=True, sharey=True)
+ax = axes.ravel()
+
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].set_title('Overlapping objects')
+ax[1].imshow(-distance, cmap=plt.cm.gray)
+ax[1].set_title('Distances')
+ax[2].imshow(labels, cmap=plt.cm.nipy_spectral)
+ax[2].set_title('Separated objects')
+
+for a in ax:
+    a.set_axis_off()
+
+fig.tight_layout()
+plt.show()
