94831c349f6332d1eb0b88baef41c83e8769ee1f
alzbeta-hrdlickova <56198872+alzbeta-hrdlickova@users.noreply.github.com>
2021-05-05 04:38:21 +0200

    Update watershed.py

--- watershed.py
+++ watershed.py
@@ -7,44 +7,18 @@ from skimage.morphology import extrema
 from skimage.measure import label
 from skimage import data, util, filters, color
 from skimage.feature import peak_local_max
-from skimage.filters import threshold_otsu
-from skimage.filters import threshold_yen
+from skimage import img_as_bool
+from skimage import img_as_float
 from skimage.morphology import remove_small_holes
 from skimage.morphology import remove_small_objects
 from SEG import SEEGacc
-
-def binar(data):
-    threshold=threshold_otsu(data)
-    for x in range(224):      
-        for y in range(224):
-            if data[x,y] >= threshold:
-                data[x,y] = 1
-            else:
-                data[x,y] = 0
-    return data
-
-def edge_binar(labels):
-    threshold2=threshold_yen(labels)
-    for x in range(224):      
-        for y in range(224):
-            if labels[x,y] > threshold2:
-                labels[x,y] = 1
-            else:
-                labels[x,y] = 0
-    return labels
-
-def edge_data(edges1):
-    for x in range(224):      
-        for y in range(224):
-            if edges1[x,y] > 1:
-                edges1[x,y] = 0
-            elif edges1[x,y] < 1:
-                edges1[x,y] = 0
-            else:
-                edges1[x,y] = 1
-    return edges1
+from skimage.segmentation import random_walker
+from skimage import measure
+from skimage.morphology import local_maxima
 
 SEGS=[]
+SEGS_binar=[]
+
 it=-1
 for fin in range(70):
     it+=1
@@ -52,42 +26,71 @@ for fin in range(70):
 
     data =np.load('/Users/betyadamkova/Desktop/final/data/' + 'data' + str(it) +'.npy') 
     data=data[0,0,:,:]
-    inverse_data=util.invert(data)              #inverzní distanšční mapa
-    h1 = 0
+    inverse_data=util.invert(data)             #inverzní distanšční mapa
+    h1 = 1                                      #hledání středů buněk, h= minimální výška extrahovaných maxim
     h1_maxima = extrema.h_maxima(data, h1)
-    label_h_maxima1 = label(h1_maxima)
-    data_edge=edge_data(data) 
-    plt.imshow(data_edge,cmap="gray")
+    #plt.imshow(h1_maxima,cmap="gray")
+    binar_data=data>0
+    #binar_data=remove_small_holes(remove_small_objects(binar_data))
+    binar_data=img_as_float(binar_data)
+    label_h_maxima1 = label(binar_data)        #označení buněk
+    plt.imshow(label_h_maxima1,cmap=plt.cm.nipy_spectral)
+    #data_edge=data==1               #okraje buněk
+    #data_edge=remove_small_holes(remove_small_objects(data_edge, 1), 1)
+    #plt.imshow(data_edge,cmap="gray")
     
     output =np.load('/Users/betyadamkova/Desktop/final/output/' + 'output' + str(it) +'.npy') 
     output=output[0,0,:,:]
-    plt.imshow(output,cmap="gray")
-    binar_output=binar(output)
-    h2 = 0.005
+    binar_output=output > -0.32                            #binární obraz dle Otsu threshold
+    binary_output=remove_small_holes(remove_small_objects(binar_output, 50),50)
+
+    h2 =0.05    
     h2_maxima = extrema.h_maxima(output, h2)
-    label_h_maxima2 = label(h2_maxima)
+    #seed=local_maxima(distance_output, selem=None, connectivity=50, indices=False, allow_borders=False)    #malezení středu buněk
+    #peak=peak_local_max(distance_output,min_distance=7, indices=False,footprint=np.ones((9,9)), labels=binar_output)
+    label_h_maxima2  = label(h2_maxima) 
+    plt.imshow(h2_maxima,cmap="gray")                                         #označení buněk
     
-    labels=watershed(inverse_data, label_h_maxima1, mask=binar_output)
-    edges = filters.sobel(labels)
-    output_edge=edge_binar(edges)
+    labels=watershed(-output, label_h_maxima2, mask=binar_output) #inverní distanční mapa, označené středy, binární
+    plt.imshow(labels,cmap=plt.cm.nipy_spectral)
     
-    seg_score=SEEGacc (labels, data, 0)
+    
+    ###########random walker for segmentation
+    label_h_maxima2[~binar_output] = -1
+    labels_rw = random_walker(binar_output, label_h_maxima2, beta=10, mode='bf')
+    plt.imshow(labels_rw,cmap=plt.cm.nipy_spectral)
+    
+    properties = measure.regionprops(labels_rw)
+    properties_labels = measure.regionprops(labels)
+    #print([prop.area for prop in properties])
+    #print([prop.area for prop in properties_labels])
+ 
+    overlay_h = color.label2rgb(labels, data, alpha=0.7, bg_label=0, bg_color=None, colors=[(1, 0, 0)],image_alpha=1)
+    #plt.imshow(overlay_h,cmap="gray");plt.axis ('off')
+    
+####################
+    seg_score=SEEGacc(labels, label_h_maxima1)
     SEGS.append(seg_score)
+    #print("SEG:", seg_score) 
+
+    seg_score3=SEEGacc(binar_output, binar_data)
+    SEGS_binar.append(seg_score3)
+    #print("SEG_binar:", seg_score3)
     
 segs_mean=sum(SEGS)/70
-print("SEG:", segs_mean) 
+segs_mean3=sum(SEGS_binar)/70
+print("Výsledek SEG:", segs_mean)  
+print("Výsledek SEG_binar:", segs_mean3) 
 
-fig, axes = plt.subplots(ncols=4, figsize=(15, 4), sharex=True, sharey=True)
+
+fig, axes = plt.subplots(ncols=3, figsize=(15, 4), sharex=True, sharey=True)
 ax = axes.ravel()
-ax[0].imshow(inverse_data, cmap=plt.cm.gray)
-ax[0].set_title('Inverzní distanční mapa')
+ax[0].imshow(label_h_maxima1, cmap=plt.cm.nipy_spectral)
+ax[0].set_title('a)')
 ax[0].axis('off')
 ax[1].imshow(labels, cmap=plt.cm.nipy_spectral)
-ax[1].set_title('Watershed')
+ax[1].set_title('b)')
 ax[1].axis('off')
-ax[2].imshow(data_edge, cmap=plt.cm.gray)
-ax[2].set_title('Edges data')
+ax[2].imshow(labels_rw, cmap=plt.cm.nipy_spectral)
+ax[2].set_title('c)')
 ax[2].axis('off')
-ax[3].imshow(output_edge, cmap=plt.cm.gray)
-ax[3].set_title('Output edge')
-ax[3].axis('off')
